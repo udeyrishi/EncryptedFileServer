3eaf63f85d61a6fc9d1235cd3dcc41c9fdfce9f5
Udey Rishi <udey.rishi@gmail.com>
2016-03-31 00:32:50 -0600

    Implemented FileTransferState

--- src/com/udeyrishi/encryptedfileserver/common/tea/TEAFileServerProtocolStandard.java
+++ src/com/udeyrishi/encryptedfileserver/common/tea/TEAFileServerProtocolStandard.java
@@ -9,15 +9,27 @@ import com.udeyrishi.encryptedfileserver.common.communication.StringMessage;
 public class TEAFileServerProtocolStandard {
 
     public static class TypeNames {
-        public static final String AUTH_RESPONSE = "Auth-Response";
         public static final String AUTH_REQUEST = "Auth-Request";
+        public static final String AUTH_RESPONSE = "Auth-Response";
+
         public static final String FILE_REQUEST = "File-Request";
+        public static final String FILE_RESPONSE_FAILURE = "File-Response-Failure";
+        public static final String FILE_RESPONSE_SUCCESS = "File-Response-Success";
+
         public static final String TERMINATION_REQUEST = "Termination-Request";
+
+        public static final String INTERRUPT_NOTIFICATION = "Interrupt";
     }
 
     public static class StandardMessages {
         public static final Message ACCESS_DENIED = new StringMessage(TypeNames.AUTH_RESPONSE, "Access-Denied");
         public static final Message ACCESS_GRANTED = new StringMessage(TypeNames.AUTH_RESPONSE, "Access-Granted");
+
+        public static final Message BAD_FILE_REQUEST_RESPONSE = new StringMessage(TypeNames.FILE_RESPONSE_FAILURE,
+                                                                                  "Bad-Request");
+        public static final Message FILE_NOT_FOUND_RESPONSE = new StringMessage(TypeNames.FILE_RESPONSE_FAILURE,
+                                                                                "File-Not-Found");
+
         public static final Message TERMINATION_REQUEST = new StringMessage(TypeNames.TERMINATION_REQUEST, null);
     }
 
--- src/com/udeyrishi/encryptedfileserver/server/fileserverstates/FileTransferState.java
+++ src/com/udeyrishi/encryptedfileserver/server/fileserverstates/FileTransferState.java
@@ -1,28 +1,55 @@
 package com.udeyrishi.encryptedfileserver.server.fileserverstates;
 
-import com.udeyrishi.encryptedfileserver.common.communication.CommunicationProtocol;
-import com.udeyrishi.encryptedfileserver.common.communication.Message;
+import com.udeyrishi.encryptedfileserver.common.communication.*;
+import com.udeyrishi.encryptedfileserver.common.tea.TEAFileServerProtocolStandard;
+import com.udeyrishi.encryptedfileserver.common.utils.Preconditions;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by rishi on 2016-03-30.
  */
 public class FileTransferState implements CommunicationProtocol.CommunicationProtocolState {
+    private final String root;
+    private String lastFileRequested = null;
+    private boolean interrupted = false;
+
     public FileTransferState(String pathToFilesRootDir) {
-
+        this.root = Preconditions.checkNotNull(pathToFilesRootDir, "pathToFilesRootDir");
     }
 
     @Override
-    public void messageReceived(CommunicationProtocol protocol, Message message) throws IllegalStateException {
-
+    public void messageReceived(CommunicationProtocol protocol, Message message) throws IOException, BadMessageException {
+        if (message.getTypeName().equals(TEAFileServerProtocolStandard.TypeNames.FILE_REQUEST)) {
+            lastFileRequested = message.getMessageContents();
+        } else if (MessageUtils.areEqual(message, TEAFileServerProtocolStandard.StandardMessages.TERMINATION_REQUEST)) {
+            protocol.setState(CommunicationProtocol.TERMINATED_STATE);
+        }
     }
 
     @Override
-    public Message nextTransmissionMessage(CommunicationProtocol protocol) throws IllegalStateException {
-        return null;
+    public Message nextTransmissionMessage(CommunicationProtocol protocol) {
+        if (interrupted) {
+            return new StringMessage(TEAFileServerProtocolStandard.TypeNames.INTERRUPT_NOTIFICATION, null);
+        }
+
+        if (lastFileRequested == null) {
+            return TEAFileServerProtocolStandard.StandardMessages.BAD_FILE_REQUEST_RESPONSE;
+        }
+
+        try {
+            return new BufferedReaderMessage(TEAFileServerProtocolStandard.TypeNames.FILE_RESPONSE_SUCCESS,
+                                             new BufferedReader(new FileReader(lastFileRequested)));
+        } catch (FileNotFoundException e) {
+            return TEAFileServerProtocolStandard.StandardMessages.FILE_NOT_FOUND_RESPONSE;
+        }
     }
 
     @Override
     public void interrupt(CommunicationProtocol protocol) {
-
+        interrupted = true;
     }
 }
