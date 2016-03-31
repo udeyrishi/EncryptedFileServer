7a43eac0fae57e1028003f09d8176e12ab5a8b74
Udey Rishi <udey.rishi@gmail.com>
2016-03-30 19:50:38 -0600

    Added ability to set message filters in the comm protocol

--- src/com/udeyrishi/encryptedfileserver/common/communication/CommunicationProtocol.java
+++ src/com/udeyrishi/encryptedfileserver/common/communication/CommunicationProtocol.java
@@ -5,34 +5,70 @@ package com.udeyrishi.encryptedfileserver.common.communication;
  */
 public class CommunicationProtocol {
     private CommunicationProtocolState state;
+    private MessageFilter filter;
+
+    public CommunicationProtocol(CommunicationProtocolState initialState, MessageFilter filter) {
+        setState(initialState);
+        setMessageFilter(filter);
+    }
 
     public CommunicationProtocol(CommunicationProtocolState initialState) {
-        this.state = initialState;
-    }
-
-    public void processReceivedMessage(Message message) {
-        state.messageReceived(this, message);
-    }
-
-    public Message getNextTransmissionMessage() {
-        return state.nextTransmissionMessage(this);
-    }
-
-    public boolean isTerminated() {
-        return state == null;
-    }
-
-    public void interrupt() {
-        state.interrupt();
+        this(initialState, null);
     }
 
     public void setState(CommunicationProtocolState state) {
         this.state = state;
     }
 
+    public void setMessageFilter(MessageFilter filter) {
+        this.filter = filter;
+    }
+
+    public void processReceivedMessage(Message message) {
+        message = filter == null ? message : filter.incomingMessageFilter(message);
+        state.messageReceived(this, message);
+    }
+
+    public Message getNextTransmissionMessage() {
+        Message message = state.nextTransmissionMessage(this);
+        message = filter == null ? message : filter.outgoingMessageFilter(message);
+        return message;
+    }
+
+    public boolean isTerminated() {
+        return state == TERMINATED_STATE;
+    }
+
+    public void interrupt() {
+        state.interrupt(this);
+    }
+
     public interface CommunicationProtocolState {
         void messageReceived(CommunicationProtocol protocol, Message message);
         Message nextTransmissionMessage(CommunicationProtocol protocol);
-        void interrupt();
+        void interrupt(CommunicationProtocol protocol);
+    }
+
+    public static final CommunicationProtocolState TERMINATED_STATE = new CommunicationProtocolState() {
+        @Override
+        public void messageReceived(CommunicationProtocol protocol, Message message) {
+            // no-op
+        }
+
+        @Override
+        public Message nextTransmissionMessage(CommunicationProtocol protocol) {
+            // Should never be called if terminated. Try to throw NullPointerException somewhere
+            return null;
+        }
+
+        @Override
+        public void interrupt(CommunicationProtocol protocol) {
+            // no-op
+        }
+    };
+
+    public interface MessageFilter {
+        Message incomingMessageFilter(Message message);
+        Message outgoingMessageFilter(Message message);
     }
 }
---
+++ src/com/udeyrishi/encryptedfileserver/common/tea/TEAMessageFilter.java
@@ -0,0 +1,28 @@
+package com.udeyrishi.encryptedfileserver.common.tea;
+
+import com.udeyrishi.encryptedfileserver.common.communication.CommunicationProtocol;
+import com.udeyrishi.encryptedfileserver.common.communication.Message;
+import com.udeyrishi.encryptedfileserver.common.utils.Preconditions;
+
+/**
+ * Created by rishi on 2016-03-30.
+ */
+public class TEAMessageFilter implements CommunicationProtocol.MessageFilter {
+
+    private final TEAKey key;
+
+    //TODO: JNI based TEA encryption/decryption
+    public TEAMessageFilter(TEAKey key) {
+        this.key = Preconditions.checkNotNull(key, "key");
+    }
+
+    @Override
+    public Message incomingMessageFilter(Message message) {
+        return message;
+    }
+
+    @Override
+    public Message outgoingMessageFilter(Message message) {
+        return message;
+    }
+}
